7c78615cf537d99b1e598578cadb3bbcc80fb61c
Kacey Coley <kcoley@users.noreply.github.com>
2017-08-18 13:00:08 -0700

    Added binary write support for Vector4

--- Source/Data.cs
+++ Source/Data.cs
@@ -58,6 +58,18 @@ namespace AssetGenerator
 
     internal static class BinaryWriterExtensions
     {
+        public static void Write(this BinaryWriter writer, Vector4 value)
+        {
+            writer.Write(value.x);
+            writer.Write(value.y);
+            writer.Write(value.z);
+            writer.Write(value.w);
+        }
+
+        public static void Write(this BinaryWriter writer, IEnumerable<Vector4> values)
+        {
+            values.ForEach(value => writer.Write(value));
+        }
         public static void Write(this BinaryWriter writer, Vector3 value)
         {
             writer.Write(value.x);
