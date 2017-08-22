c331f8ce5bbfe487542f18232aa6c5d96b809899
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-08-22 13:32:52 -0700

    Save test values in objects and generate powerset of models to create

--- Source/Program.cs
+++ Source/Program.cs
@@ -12,44 +12,55 @@ namespace AssetGenerator
             var executingAssembly = Assembly.GetExecutingAssembly();
             var executingAssemblyFolder = Path.GetDirectoryName(executingAssembly.Location);
 
-            foreach (var type in executingAssembly.GetTypes())
+            TestValues makeTest = new TestValues();
+            var combos = makeTest.ParameterCombos();
+
+            foreach (var combo in combos)
             {
-                var assetGroupAttribute = type.GetCustomAttribute<AssetGroupAttribute>();
-                if (assetGroupAttribute != null)
+                string name = makeTest.GenerateName(combo);
+
+                var gltf = new Gltf
                 {
-                    foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Static))
+                    Asset = new Asset
                     {
-                        var assetAttribute = method.GetCustomAttribute<AssetAttribute>();
-                        if (assetAttribute != null)
+                        Generator = "glTF Asset Generator " + name,
+                        Version = "2.0",
+                    }
+                };
+
+                var dataList = new List<Data>();
+
+                var geometryData = new Data(name + ".bin");
+                dataList.Add(geometryData);
+
+                Common.SingleTriangle(gltf, geometryData);
+
+                gltf.Materials = new[]
+                {
+                    new Material
+                    {
+                        PbrMetallicRoughness = new MaterialPbrMetallicRoughness
                         {
-                            var gltf = new Gltf
-                            {
-                                Asset = new Asset
-                                {
-                                    Generator = "glTF Asset Generator " + Assembly.GetExecutingAssembly().GetName().Version,
-                                    Version = "2.0",
-                                }
-                            };
-
-                            var dataList = new List<Data>();
-
-                            method.Invoke(null, new object[] { assetAttribute.Name, gltf, dataList });
-
-                            var assetFolder = Path.Combine(executingAssemblyFolder, assetGroupAttribute.Folder);
-                            Directory.CreateDirectory(assetFolder);
-
-                            var assetFile = Path.Combine(assetFolder, assetAttribute.Name + ".gltf");
-                            glTFLoader.Interface.SaveModel(gltf, assetFile);
-
-                            foreach (var data in dataList)
-                            {
-                                data.Writer.Flush();
-
-                                var dataFile = Path.Combine(assetFolder, data.Name);
-                                File.WriteAllBytes(dataFile, ((MemoryStream)data.Writer.BaseStream).ToArray());
-                            }
+                            // MAKE THIS USE THE NEW CLASS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                            BaseColorFactor = new[] { 1.0f, 0.0f, 0.0f, 0.0f },
                         }
                     }
+                };
+
+                gltf.Meshes[0].Primitives[0].Material = 0;
+
+                var assetFolder = Path.Combine(executingAssemblyFolder, executingAssemblyFolder + "Materials");
+                Directory.CreateDirectory(assetFolder);
+
+                var assetFile = Path.Combine(assetFolder, name + ".gltf");
+                glTFLoader.Interface.SaveModel(gltf, assetFile);
+
+                foreach (var data in dataList)
+                {
+                    data.Writer.Flush();
+
+                    var dataFile = Path.Combine(assetFolder, data.Name);
+                    File.WriteAllBytes(dataFile, ((MemoryStream)data.Writer.BaseStream).ToArray());
                 }
             }
         }
---
+++ Source/TestValues.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using glTFLoader.Schema;
+using static AssetGenerator.GLTFWrapper;
+
+namespace AssetGenerator
+{
+    public class TestValues
+    {
+        public TestValues()
+        {
+
+        }
+
+        public Parameter[][] ParameterCombos()
+        {
+            //List<Parameter> parameters = new List<Parameter>
+            Parameter[] parameters =
+            {
+                new Parameter("BaseColorFactor", new[] { 1.0f, 0.0f, 0.0f, 0.0f }),
+                new Parameter("BetallicFactor", 0.5f),
+                new Parameter("RoughnessFactor", 0.5f)
+            };
+
+            var temp = PowerSet<Parameter>(parameters);
+
+            return temp;
+        }
+
+        //https://stackoverflow.com/questions/19890781/creating-a-power-set-of-a-sequence
+        public static T[][] PowerSet<T>(T[] seq)
+        {
+            var powerSet = new T[1 << seq.Length][];
+            powerSet[0] = new T[0]; // starting only with empty set
+            for (int i = 0; i < seq.Length; i++)
+            {
+                var cur = seq[i];
+                int count = 1 << i; // doubling list each time
+                for (int j = 0; j < count; j++)
+                {
+                    var source = powerSet[j];
+                    var destination = powerSet[count + j] = new T[source.Length + 1];
+                    for (int q = 0; q < source.Length; q++)
+                        destination[q] = source[q];
+                    destination[source.Length] = cur;
+                }
+            }
+            return powerSet;
+        }
+
+        public string GenerateName(Parameter[] paramSet)
+        {
+            string name = null;
+
+            for (int i = 0; i < paramSet.Length; i++)
+            {
+                name += paramSet[i].name;
+            }
+
+            if (name == null)
+            {
+                name = "None";
+            }
+
+            return name;
+        }
+    }
+
+    public class Parameter
+    {
+        public string name { get; }
+        public dynamic value; // Could be a float, array of floats, or string
+        public bool required;
+
+        public Parameter(string parmName, dynamic parameterValue)
+        {
+            name = parmName;
+            value = parameterValue;
+            required = false;
+        }
+    }
+}
