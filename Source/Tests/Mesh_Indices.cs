120f73b383667657c593ab08941c28847947d608
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-11-16 16:18:01 -0800

    Added vertexes appropriate for all modes

--- Source/Tests/Mesh_Indices.cs
+++ Source/Tests/Mesh_Indices.cs
@@ -15,44 +15,65 @@ namespace AssetGenerator.Tests
                 Uri = icon_Indices
             };
             usedImages.Add(iconIndices);
-            List<Vector3> primitiveNoIndicesPositions = new List<Vector3>()
+            List<Vector3> noIndicesPositionsTriangles = new List<Vector3>()
+            {
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+            };
+            List<Vector3> noIndicesPositionsLines = new List<Vector3>()
+            {
+                new Vector3( 0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f,-0.5f, 0.0f),
+            };
+            List<Vector3> noIndicesPositionsLineloopPointsFan = new List<Vector3>()
+            {
+                new Vector3( 0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+            };
+            List<Vector3> noIndicesPositionsLineStrip = new List<Vector3>()
+            {
+                new Vector3( 0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3( 0.5f,-0.5f, 0.0f),
+            };
+            List<Vector3> noIndicesPositionsTrianglestrip = new List<Vector3>()
             {
                 new Vector3( 0.5f,-0.5f, 0.0f),
                 new Vector3( 0.5f, 0.5f, 0.0f),
                 new Vector3(-0.5f,-0.5f, 0.0f),
-                new Vector3(-0.5f,-0.5f, 0.0f),
-                new Vector3( 0.5f, 0.5f, 0.0f),
-                new Vector3(-0.5f, 0.5f, 0.0f)
+                new Vector3(-0.5f, 0.5f, 0.0f),
             };
             Runtime.GLTF defaultModel = Common.SinglePlane(); // Only used to get the default indices
             List<int> defaultModelIndices = new List<int>(defaultModel.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Indices);
-            List<int> primitiveTriangleIndices = new List<int>
-            {
-                0, 1, 2,
-            };
             List<int> linesIndices = new List<int>
             {
                 0, 3, 3, 2, 2, 1, 1, 0,
             };
-            List<int> lineLoopAndPointsIndices = new List<int>
+            List<int> lineloopPointsFanIndices = new List<int>
             {
                 0, 3, 2, 1,
             };
-            List<int> lineStripIndices = new List<int>
+            List<int> linestripIndices = new List<int>
             {
                 0, 3, 2, 1, 0,
             };
-            List<int> triangleStripIndices = new List<int>
+            List<int> trianglestripIndices = new List<int>
             {
                 0, 3, 1, 2,
-            };
-            List<int> triangleFanIndices = new List<int>
-            {
-                0, 3, 2, 1,
-            };
-            Runtime.MeshPrimitive primitiveNoIndicesMesh = new Runtime.MeshPrimitive
-            {
-                Positions = primitiveNoIndicesPositions,
             };
             List<Vector4> vertexColors = new List<Vector4>()
             {
@@ -69,12 +90,12 @@ namespace AssetGenerator.Tests
                 new Property(Propertyname.Mode_Triangle_Strip, Runtime.MeshPrimitive.ModeEnum.TRIANGLE_STRIP, group: 1),
                 new Property(Propertyname.Mode_Triangle_Fan, Runtime.MeshPrimitive.ModeEnum.TRIANGLE_FAN, group: 1),
                 new Property(Propertyname.Mode_Triangles, Runtime.MeshPrimitive.ModeEnum.TRIANGLES, group: 1),
-                new Property(Propertyname.IndicesValues_Points, lineLoopAndPointsIndices, Propertyname.Mode_Points, group: 2),
+                new Property(Propertyname.IndicesValues_Points, lineloopPointsFanIndices, Propertyname.Mode_Points, group: 2),
                 new Property(Propertyname.IndicesValues_Lines, linesIndices, Propertyname.Mode_Lines, group: 2),
-                new Property(Propertyname.IndicesValues_LineLoop, lineLoopAndPointsIndices, Propertyname.Mode_Line_Loop, group: 2),
-                new Property(Propertyname.IndicesValues_LineStrip, lineStripIndices, Propertyname.Mode_Line_Strip, group: 2),
-                new Property(Propertyname.IndicesValues_TriangleStrip, triangleStripIndices, Propertyname.Mode_Triangle_Strip, group: 2),
-                new Property(Propertyname.IndicesValues_TriangleFan, triangleFanIndices, Propertyname.Mode_Triangle_Fan, group: 2),
+                new Property(Propertyname.IndicesValues_LineLoop, lineloopPointsFanIndices, Propertyname.Mode_Line_Loop, group: 2),
+                new Property(Propertyname.IndicesValues_LineStrip, linestripIndices, Propertyname.Mode_Line_Strip, group: 2),
+                new Property(Propertyname.IndicesValues_TriangleStrip, trianglestripIndices, Propertyname.Mode_Triangle_Strip, group: 2),
+                new Property(Propertyname.IndicesValues_TriangleFan, lineloopPointsFanIndices, Propertyname.Mode_Triangle_Fan, group: 2),
                 new Property(Propertyname.IndicesValues_Triangles, defaultModelIndices, Propertyname.Mode_Triangles, group: 2),
                 new Property(Propertyname.IndicesComponentType_Byte, Runtime.MeshPrimitive.IndexComponentTypeEnum.UNSIGNED_BYTE, group: 4),
                 new Property(Propertyname.IndicesComponentType_Short, Runtime.MeshPrimitive.IndexComponentTypeEnum.UNSIGNED_SHORT, group: 4),
@@ -83,8 +104,14 @@ namespace AssetGenerator.Tests
             };
             specialProperties = new List<Property>
             {
-                new Property(Propertyname.IndicesComponentType_None, primitiveNoIndicesMesh, group: 2),
                 new Property(Propertyname.VertexColor_Vector4_Float, vertexColors),
+                new Property(Propertyname.Mode_Points, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.Mode_Lines, noIndicesPositionsLines, group: 1),
+                new Property(Propertyname.Mode_Line_Loop, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.Mode_Line_Strip, noIndicesPositionsLineStrip, group: 1),
+                new Property(Propertyname.Mode_Triangle_Strip, noIndicesPositionsTrianglestrip, group: 1),
+                new Property(Propertyname.Mode_Triangle_Fan, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.Mode_Triangles, noIndicesPositionsTriangles, group: 1),
             };
             // Each mode with and without indices, and drop singles
             var defaultIndices = properties.Find(e => e.name == Propertyname.IndicesComponentType_Int);
@@ -192,8 +219,10 @@ namespace AssetGenerator.Tests
                 }
                 else if (property.name == Propertyname.IndicesComponentType_None)
                 {
-                    var mesh = specialProperties.Find(e => e.name == Propertyname.IndicesComponentType_None);
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Positions = mesh.value.Positions;
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Indices = null;
+                    var mode = combo.Find(e => e.propertyGroup == 1);
+                    var modeVertexes = specialProperties.Find(e => e.name == mode.name);
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Positions = modeVertexes.value;
                 }
             }
 
