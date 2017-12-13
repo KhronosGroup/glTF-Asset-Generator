41d2c704d99fe2604a9cdc38a2d62d91e7ff1971
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-13 15:31:20 -0800

    Action feedback on log

--- Source/LogTemplates/Node.md
+++ Source/LogTemplates/Node.md
@@ -1,7 +1,5 @@
 These models contain the basic attributes of the node level object.
 
-There are two camera objects in each model, but the table below shows which camera the index is set to.
-
 The following table shows the properties that are set for a given model.  
 
 ~~Table~~ 
--- Source/ModelGroups/Node.cs
+++ Source/ModelGroups/Node.cs
@@ -12,15 +12,34 @@ namespace AssetGenerator.ModelGroups
             onlyBinaryProperties = false;
             properties = new List<Property>
             {
-                new Property(Propertyname.Camera_0, "Camera 0", group: 1),
-                new Property(Propertyname.Camera_1, "Camera 1", group: 1),
-                new Property(Propertyname.Matrix, null),
-                new Property(Propertyname.Mesh0, null),
-                new Property(Propertyname.Mesh1, null),
+                new Property(Propertyname.Matrix_Positive, "Positive", group: 1),
+                new Property(Propertyname.Matrix_Negative, "Negative", group: 1),
                 new Property(Propertyname.Rotation, null),
-                new Property(Propertyname.Scale, null),
+                new Property(Propertyname.Scale_Positive, "Positive", group: 2),
+                new Property(Propertyname.Scale_Negative, "Negative", group: 2),
                 new Property(Propertyname.Translation, null),
             };
+            var matrixP = properties.Find(e => e.name == Propertyname.Matrix_Positive);
+            var matrixN = properties.Find(e => e.name == Propertyname.Matrix_Negative);
+            var rotation = properties.Find(e => e.name == Propertyname.Rotation);
+            var scaleP = properties.Find(e => e.name == Propertyname.Scale_Positive);
+            var scaleN= properties.Find(e => e.name == Propertyname.Scale_Negative);
+            var translation = properties.Find(e => e.name == Propertyname.Translation);
+            specialCombos.Add(new List<Property>()
+            {
+                translation,
+                matrixP
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                scaleP,
+                matrixP
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                rotation,
+                matrixP
+            });
         }
 
         public Runtime.GLTF SetModelAttributes(Runtime.GLTF wrapper, Runtime.Material material, List<Property> combo, ref glTFLoader.Schema.Gltf gltf)
