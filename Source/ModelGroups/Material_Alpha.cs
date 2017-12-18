42e2924db3426bf0158268136a664ac5f004a52b
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-18 10:34:30 -0800

    Add more models to test different values for alpha cutoff

--- Source/ModelGroups/Material_Alpha.cs
+++ Source/ModelGroups/Material_Alpha.cs
@@ -28,9 +28,11 @@ namespace AssetGenerator.ModelGroups
                 new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:2),
                 new Property(Propertyname.AlphaMode_Mask, glTFLoader.Schema.Material.AlphaModeEnum.MASK, group:1),
                 new Property(Propertyname.AlphaMode_Blend, glTFLoader.Schema.Material.AlphaModeEnum.BLEND, group:1),
-                new Property(Propertyname.AlphaCutoff, 0.7f),
+                new Property(Propertyname.AlphaCutoff_Low, 0.6f,  group:3),
+                new Property(Propertyname.AlphaCutoff_High, 0.8f,  group:3),
+                new Property(Propertyname.AlphaCutoff_Equal, 0.7f,  group:3),
                 new Property(Propertyname.DoubleSided, true),
-                new Property(Propertyname.BaseColorFactor, new Vector4(1.0f, 1.0f, 1.0f, 0.6f)),
+                new Property(Propertyname.BaseColorFactor, new Vector4(1.0f, 1.0f, 1.0f, 0.7f)),
                 new Property(Propertyname.BaseColorTexture, baseColorTexture),
             };
             specialProperties = new List<Property>
@@ -38,12 +40,45 @@ namespace AssetGenerator.ModelGroups
                 new Property(Propertyname.BaseColorTexture, baseColorTexture),
                 new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:2),
             };
+            var mask = properties.Find(e => e.name == Propertyname.AlphaMode_Mask);
+            var alphaCutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
+            var alphaCutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
+            var alphaCutoffEqual = properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal);
+            var baseColorFactor = properties.Find(e => e.name == Propertyname.BaseColorFactor);
             specialCombos.Add(ComboHelper.CustomComboCreation(
                 properties.Find(e => e.name == Propertyname.AlphaMode_Mask),
-                properties.Find(e => e.name == Propertyname.AlphaCutoff)));
+                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low)));
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffEqual
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffHigh
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffLow,
+                baseColorFactor
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffEqual,
+                baseColorFactor
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffHigh,
+                baseColorFactor
+            });
             specialCombos.Add(ComboHelper.CustomComboCreation(
                 properties.Find(e => e.name == Propertyname.DoubleSided),
-                properties.Find(e => e.name == Propertyname.AlphaCutoff),
+                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low),
                 properties.Find(e => e.name == Propertyname.AlphaMode_Mask)));
             specialCombos.Add(ComboHelper.CustomComboCreation(
                 properties.Find(e => e.name == Propertyname.DoubleSided),
@@ -57,7 +92,11 @@ namespace AssetGenerator.ModelGroups
                 properties.Find(e => e.name == Propertyname.AlphaMode_Blend),
                 properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float)));
             removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaCutoff)));
+                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low)));
+            removeCombos.Add(ComboHelper.CustomComboCreation(
+                properties.Find(e => e.name == Propertyname.AlphaCutoff_High)));
+            removeCombos.Add(ComboHelper.CustomComboCreation(
+                properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal)));
             removeCombos.Add(ComboHelper.CustomComboCreation(
                 properties.Find(e => e.name == Propertyname.BaseColorFactor)));
             removeCombos.Add(ComboHelper.CustomComboCreation(
@@ -68,8 +107,15 @@ namespace AssetGenerator.ModelGroups
 
         override public List<List<Property>> ApplySpecialProperties(ModelGroup test, List<List<Property>> combos)
         {
+            var blend = properties.Find(e => e.name == Propertyname.AlphaMode_Mask);
+            var mask = properties.Find(e => e.name == Propertyname.AlphaMode_Mask);
+            var alphaCutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
+            var alphaCutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
+            var alphaCutoffEqual = properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal);
+            var baseColorFactor = properties.Find(e => e.name == Propertyname.BaseColorFactor);
+            var baseColorTexture = properties.Find(e => e.name == Propertyname.BaseColorTexture);
+
             // BaseColorTexture is used everywhere except in the empty set
-            var baseColorTexture = specialProperties.Find(e => e.name == Propertyname.BaseColorTexture);
             foreach (var y in combos)
             {
                 // Checks if the property is already in that combo, or vertexcolor
@@ -93,13 +139,35 @@ namespace AssetGenerator.ModelGroups
                 properties.Find(e => e.name == Propertyname.AlphaMode_Mask),
                 properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float)));
 
-            // Add two combos to the bottom, so they don't have a base color texture
-            combos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaMode_Blend),
-                properties.Find(e => e.name == Propertyname.BaseColorFactor)));
-            combos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaMode_Mask),
-                properties.Find(e => e.name == Propertyname.BaseColorFactor)));
+            // Add more combos last, so they don't have a base color texture
+            combos.Add(new List<Property>()
+            {
+                blend,
+                baseColorFactor
+            });
+            combos.Add(new List<Property>()
+            {
+                mask,
+                baseColorFactor
+            });
+            combos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffLow,
+                baseColorFactor,
+            });
+            combos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffEqual,
+                baseColorFactor
+            });
+            combos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffHigh,
+                baseColorFactor,
+            });
 
             return combos;
         }
@@ -115,7 +183,7 @@ namespace AssetGenerator.ModelGroups
                 {
                     material.AlphaMode = property.value;
                 }
-                else if (property.name == Propertyname.AlphaCutoff)
+                else if (property.propertyGroup == 3) // Alpha Cutoff
                 {
                     material.AlphaCutoff = property.value;
                 }
