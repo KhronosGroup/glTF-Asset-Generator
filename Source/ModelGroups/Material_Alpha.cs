d02d20e1ac4f42af746d3df62c4557b9e25275c1
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-18 11:13:20 -0800

    Add double sided models with normals

--- Source/ModelGroups/Material_Alpha.cs
+++ Source/ModelGroups/Material_Alpha.cs
@@ -15,13 +15,32 @@ namespace AssetGenerator.ModelGroups
             {
                 Uri = texture_BaseColor
             };
+            Runtime.Image normalTexture = new Runtime.Image
+            {
+                Uri = texture_Normal
+            };
             usedImages.Add(baseColorTexture);
+            usedImages.Add(normalTexture);
             List<Vector4> vertexColors = new List<Vector4>()
             {
                 new Vector4( 0.3f, 0.3f, 0.3f, 0.4f),
                 new Vector4( 0.3f, 0.3f, 0.3f, 0.2f),
                 new Vector4( 0.3f, 0.3f, 0.3f, 0.8f),
                 new Vector4( 0.3f, 0.3f, 0.3f, 0.6f)
+            };
+            List<Vector3> planeNormals = new List<Vector3>()
+            {
+                new Vector3( 0.0f, 0.0f,1.0f),
+                new Vector3( 0.0f, 0.0f,1.0f),
+                new Vector3( 0.0f, 0.0f,1.0f),
+                new Vector3( 0.0f, 0.0f,1.0f)
+            };
+            List<Vector4> tangents = new List<Vector4>()
+            {
+                new Vector4( 1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, 0.0f, 0.0f, 1.0f),
+                new Vector4( 1.0f, 0.0f, 0.0f, 1.0f)
             };
             properties = new List<Property>
             {
@@ -33,6 +52,9 @@ namespace AssetGenerator.ModelGroups
                 new Property(Propertyname.AlphaCutoff_Equal, 0.7f,  group:3),
                 new Property(Propertyname.DoubleSided, true),
                 new Property(Propertyname.BaseColorFactor, new Vector4(1.0f, 1.0f, 1.0f, 0.7f)),
+                new Property(Propertyname.VertexNormal, planeNormals),
+                new Property(Propertyname.VertexTangent, tangents),
+                new Property(Propertyname.NormalTexture, normalTexture),
                 new Property(Propertyname.BaseColorTexture, baseColorTexture),
             };
             specialProperties = new List<Property>
@@ -41,13 +63,22 @@ namespace AssetGenerator.ModelGroups
                 new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:2),
             };
             var mask = properties.Find(e => e.name == Propertyname.AlphaMode_Mask);
+            var blend = properties.Find(e => e.name == Propertyname.AlphaMode_Blend);
             var alphaCutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
             var alphaCutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
             var alphaCutoffEqual = properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal);
             var baseColorFactor = properties.Find(e => e.name == Propertyname.BaseColorFactor);
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaMode_Mask),
-                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low)));
+            var colorTex = properties.Find(e => e.name == Propertyname.BaseColorTexture);
+            var color = properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float);
+            var doubleSided = properties.Find(e => e.name == Propertyname.DoubleSided);
+            var normal = properties.Find(e => e.name == Propertyname.VertexNormal);
+            var tangent = properties.Find(e => e.name == Propertyname.VertexTangent);
+            var normTex = properties.Find(e => e.name == Propertyname.NormalTexture);
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                alphaCutoffLow,
+            });
             specialCombos.Add(new List<Property>()
             {
                 mask,
@@ -76,33 +107,89 @@ namespace AssetGenerator.ModelGroups
                 alphaCutoffHigh,
                 baseColorFactor
             });
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.DoubleSided),
-                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low),
-                properties.Find(e => e.name == Propertyname.AlphaMode_Mask)));
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.DoubleSided),
-                properties.Find(e => e.name == Propertyname.AlphaMode_Blend)));
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaMode_Mask),
-                properties.Find(e => e.name == Propertyname.BaseColorFactor)));
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.BaseColorFactor),
-                properties.Find(e => e.name == Propertyname.BaseColorTexture),
-                properties.Find(e => e.name == Propertyname.AlphaMode_Blend),
-                properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaCutoff_Low)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaCutoff_High)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.BaseColorFactor)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float)));
-            removeCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.BaseColorTexture)));
+            specialCombos.Add(new List<Property>()
+            {
+                doubleSided,
+                normal,
+                tangent,
+                normTex
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                doubleSided,
+                alphaCutoffLow,
+                mask,
+                normal,
+                tangent,
+                normTex
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                doubleSided,
+                blend,
+                normal,
+                tangent,
+                normTex
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                doubleSided,
+                alphaCutoffLow,
+                mask
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                doubleSided,
+                blend
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                mask,
+                baseColorFactor
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                baseColorFactor,
+                colorTex,
+                blend,
+                color
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 alphaCutoffLow
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 alphaCutoffEqual
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 alphaCutoffHigh
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 baseColorFactor
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 color
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 colorTex
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 normal
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 tangent
+            });
+            removeCombos.Add(new List<Property>()
+            {
+                 normTex
+            });
         }
 
         override public List<List<Property>> ApplySpecialProperties(ModelGroup test, List<List<Property>> combos)
@@ -177,9 +264,7 @@ namespace AssetGenerator.ModelGroups
 
             foreach (Property property in combo)
             {
-                if (property.name == Propertyname.AlphaMode_Opaque ||
-                    property.name == Propertyname.AlphaMode_Mask ||
-                    property.name == Propertyname.AlphaMode_Blend)
+                if (property.propertyGroup == 1) // Alpha Mode
                 {
                     material.AlphaMode = property.value;
                 }
@@ -213,6 +298,20 @@ namespace AssetGenerator.ModelGroups
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT;
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
+                }
+                else if (property.name == Propertyname.VertexNormal)
+                {
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Normals = property.value;
+                }
+                else if (property.name == Propertyname.VertexTangent)
+                {
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Tangents = property.value;
+                }
+                else if (property.name == Propertyname.NormalTexture)
+                {
+                    material.NormalTexture = new Runtime.Texture();
+                    material.NormalTexture.Source = property.value;
+                    material.NormalTexture.TexCoordIndex = 0;
                 }
             }
             wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Material = material;
