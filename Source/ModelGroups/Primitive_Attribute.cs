b486418a7ae71addce5b0efdffcb6029f8c08ef4
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-21 13:18:06 -0800

    Remove UV1 from Primitive_Attribute

--- Source/ModelGroups/Primitive_Attribute.cs
+++ Source/ModelGroups/Primitive_Attribute.cs
@@ -19,18 +19,8 @@ namespace AssetGenerator.ModelGroups
             {
                 Uri = texture_BaseColor
             };
-            Runtime.Image uvIcon0 = new Runtime.Image
-            {
-                Uri = icon_UVspace0
-            };
-            Runtime.Image uvIcon1 = new Runtime.Image
-            {
-                Uri = icon_UVspace1
-            };
             usedImages.Add(normalTexture);
             usedImages.Add(baseColorTexture);
-            usedImages.Add(uvIcon0);
-            usedImages.Add(uvIcon1);
             List<Vector3> planeNormals = new List<Vector3>()
             {
                 new Vector3( 0.0f, 0.0f,1.0f),
@@ -68,12 +58,6 @@ namespace AssetGenerator.ModelGroups
                     Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.NORMALIZED_UBYTE, group:1),
                 new Property(Propertyname.VertexUV0_Short, 
                     Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.NORMALIZED_USHORT, group:1),
-                new Property(Propertyname.VertexUV1_Float, 
-                    Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.FLOAT, Propertyname.VertexUV0_Float, 2),
-                new Property(Propertyname.VertexUV1_Byte, 
-                    Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.NORMALIZED_UBYTE, Propertyname.VertexUV0_Byte, 2),
-                new Property(Propertyname.VertexUV1_Short, 
-                    Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.NORMALIZED_USHORT, Propertyname.VertexUV0_Short, 2),
                 new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:3),
                 new Property(Propertyname.VertexNormal, planeNormals),
                 new Property(Propertyname.VertexTangent, tangents),
@@ -88,7 +72,6 @@ namespace AssetGenerator.ModelGroups
                     Runtime.MeshPrimitive.TextureCoordsComponentTypeEnum.FLOAT, group:1)
             };
             var uv0 = properties.Find(e => e.name == Propertyname.VertexUV0_Float);
-            var uv1 = properties.Find(e => e.name == Propertyname.VertexUV1_Float);
             var normal = properties.Find(e => e.name == Propertyname.VertexNormal);
             var tangent = properties.Find(e => e.name == Propertyname.VertexTangent);
             var normalTex = properties.Find(e => e.name == Propertyname.NormalTexture);
@@ -96,49 +79,18 @@ namespace AssetGenerator.ModelGroups
             var color = properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float);
             specialCombos.Add(new List<Property>()
             {
-                uv0,
-                uv1,
-                normalTex,
-            });
-            specialCombos.Add(new List<Property>()
-            {
-                uv0,
-                uv1,
                 normal,
                 normalTex,
-            });
-            specialCombos.Add(new List<Property>()
-            {
-                uv0,
-                uv1,
-                normal,
-                tangent,
-                normalTex,
-            });
-            specialCombos.Add(new List<Property>()
-            {
-                uv0,
-                normalTex,
-                colorTex
             });
             specialCombos.Add(new List<Property>()
             {
                 normal,
                 tangent,
-                normalTex,
-            });
-            specialCombos.Add(new List<Property>()
-            {
-                normal,
                 normalTex,
             });
             removeCombos.Add(new List<Property>()
             {
                 tangent,
-            });
-            removeCombos.Add(new List<Property>()
-            {
-                normalTex,
             });
             removeCombos.Add(new List<Property>()
             {
@@ -152,14 +104,12 @@ namespace AssetGenerator.ModelGroups
 
         override public List<List<Property>> ApplySpecialProperties(ModelGroup test, List<List<Property>> combos)
         {
-            // BaseColorTexture is used everywhere except in the empty set and with vertexcolor
+            // BaseColorTexture is used everywhere except in the empty set 
             var baseColorTexture = specialProperties.Find(e => e.name == Propertyname.BaseColorTexture);
             foreach (var y in combos)
             {
-                // Checks if the property is already in that combo, or vertexcolor
-                if ((y.Find(e => e.name == baseColorTexture.name)) == null &&
-                    (y.Find(e => LogStringHelper.GenerateNameWithSpaces(e.name.ToString()) ==
-                    LogStringHelper.GenerateNameWithSpaces(Propertyname.VertexColor_Vector3_Float.ToString()))) == null)
+                // Checks if the property is already in that combo
+                if ((y.Find(e => e.name == baseColorTexture.name)) == null)
                 {
                     // Skip the empty set
                     if (y.Count > 0)
@@ -191,8 +141,6 @@ namespace AssetGenerator.ModelGroups
             {
                 if (x.Count == 0) return -1; // Empty Set
                 else if (y.Count == 0) return 1; // Empty Set
-                else if (x.Count == 7) return -1; // Contains all properties
-                else if (y.Count == 7) return 1; // Contains all properties
                 else if (x.Count > y.Count) return 1;
                 else if (x.Count < y.Count) return -1;
                 else if (x.Count == y.Count)
@@ -265,51 +213,9 @@ namespace AssetGenerator.ModelGroups
                 {
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordsComponentType = property.value;
                 }
-                else if (property.name == Propertyname.VertexUV1_Float ||
-                         property.name == Propertyname.VertexUV1_Byte ||
-                         property.name == Propertyname.VertexUV1_Short)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordsComponentType = property.value;
-
-                    if (wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets.Count < 2)
-                    {
-                        wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets.Add(
-                        specialProperties.Find(e => e.name == Propertyname.TexCoord).value);
-                    }
-                }
-                else if (property.name == Propertyname.VertexColor_Vector3_Float)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                }
                 else if (property.name == Propertyname.VertexColor_Vector4_Float)
                 {
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                }
-                else if (property.name == Propertyname.VertexColor_Vector3_Byte)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                }
-                else if (property.name == Propertyname.VertexColor_Vector4_Byte)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                }
-                else if (property.name == Propertyname.VertexColor_Vector3_Short)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                }
-                else if (property.name == Propertyname.VertexColor_Vector4_Short)
-                {
-                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT;
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
                 }
@@ -317,18 +223,6 @@ namespace AssetGenerator.ModelGroups
             if (combo.Count > 0) // Don't set the material on the empty set
             {
                 wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Material = material;
-            }
-            // Use the second UV if it has been set
-            if (wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets.Count > 1)
-            {
-                if (material.NormalTexture != null)
-                {
-                    material.NormalTexture.TexCoordIndex = 1;
-                }
-                if (material.MetallicRoughnessMaterial.BaseColorTexture != null)
-                {
-                    material.MetallicRoughnessMaterial.BaseColorTexture.TexCoordIndex = 1;
-                }
             }
 
             return wrapper;
