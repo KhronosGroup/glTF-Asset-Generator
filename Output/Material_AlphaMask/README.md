6e6fd83611b5ed51a1c9fbee190aae6aa181983a
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-22 15:16:15 -0800

    Action feedback

--- Output/Material_AlphaMask/README.md
+++ Output/Material_AlphaMask/README.md
@@ -11,16 +11,15 @@ Alpha Mode | Mask
 The following table shows the properties that are set for a given model.  
 
 
-Index | Alpha Cutoff | Vertex Color | Base Color Factor | Base Color Texture
-:---: | :---: | :---: | :---: | :---:
-[00](./Material_AlphaMask_00.gltf) |   |   |   |  
-[01](./Material_AlphaMask_01.gltf) |   | Vector4 Float |   |  
-[02](./Material_AlphaMask_02.gltf) |   |   |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[03](./Material_AlphaMask_03.gltf) | 0.6 |   |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[04](./Material_AlphaMask_04.gltf) | 0.7 |   |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[05](./Material_AlphaMask_05.gltf) | 0.8 |   |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[06](./Material_AlphaMask_06.gltf) | 0.6 |   | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[07](./Material_AlphaMask_07.gltf) | 0.7 |   | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[08](./Material_AlphaMask_08.gltf) | 0.8 |   | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
-[09](./Material_AlphaMask_09.gltf) | 0.6 | Vector4 Float | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+Index | Alpha Cutoff | Base Color Factor | Base Color Texture
+:---: | :---: | :---: | :---:
+[00](./Material_AlphaMask_00.gltf) |   |   |  
+[01](./Material_AlphaMask_01.gltf) |   |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[02](./Material_AlphaMask_02.gltf) | 0.4 |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[03](./Material_AlphaMask_03.gltf) | 0.7 |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[04](./Material_AlphaMask_04.gltf) | 1.1 |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[05](./Material_AlphaMask_05.gltf) | 0.0 |   | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[06](./Material_AlphaMask_06.gltf) | 0.4 | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[07](./Material_AlphaMask_07.gltf) | 0.7 | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
+[08](./Material_AlphaMask_08.gltf) | 0.5 | [1, 1, 1, 0.7] | <img src="./Texture_baseColor.png" height="72" width="72" align="middle">
  
--- Source/ModelGroups/Material_AlphaMask.cs
+++ Source/ModelGroups/Material_AlphaMask.cs
@@ -16,44 +16,34 @@ namespace AssetGenerator.ModelGroups
                 Uri = texture_BaseColor
             };
             usedImages.Add(baseColorTexture);
-            List<Vector4> vertexColors = new List<Vector4>()
-            {
-                new Vector4( 0.3f, 0.3f, 0.3f, 0.4f),
-                new Vector4( 0.3f, 0.3f, 0.3f, 0.2f),
-                new Vector4( 0.3f, 0.3f, 0.3f, 0.8f),
-                new Vector4( 0.3f, 0.3f, 0.3f, 0.6f)
-            };
             requiredProperty = new List<Property>
             {
                 new Property(Propertyname.AlphaMode_Mask, glTFLoader.Schema.Material.AlphaModeEnum.MASK)
             };
             properties = new List<Property>
             {
-                new Property(Propertyname.AlphaCutoff_Low, 0.6f,  group:3),
-                new Property(Propertyname.AlphaCutoff_Equal, 0.7f,  group:3),
-                new Property(Propertyname.AlphaCutoff_High, 0.8f,  group:3),
-                new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:2),
+                new Property(Propertyname.AlphaCutoff_Low, 0.4f,  group:3),
+                new Property(Propertyname.AlphaCutoff_High, 0.7f,  group:3),
+                new Property(Propertyname.AlphaCutoff_Multiplied, 0.5f,  group:3),
+                new Property(Propertyname.AlphaCutoff_All, 1.1f,  group:3),
+                new Property(Propertyname.AlphaCutoff_None, 0f,  group:3),
                 new Property(Propertyname.BaseColorFactor, new Vector4(1.0f, 1.0f, 1.0f, 0.7f)),
                 new Property(Propertyname.BaseColorTexture, baseColorTexture),
             };
-            var alphaCutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
-            var alphaCutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
-            var alphaCutoffEqual = properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal);
+            var cutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
+            var cutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
+            var cutoffMultiplied = properties.Find(e => e.name == Propertyname.AlphaCutoff_Multiplied);
+            var cutoffAll = properties.Find(e => e.name == Propertyname.AlphaCutoff_All);
+            var cutoffNone = properties.Find(e => e.name == Propertyname.AlphaCutoff_None);
             var baseColorFactor = properties.Find(e => e.name == Propertyname.BaseColorFactor);
-            var color = properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float);
             specialCombos.Add(new List<Property>()
             {
-                alphaCutoffLow,
+                cutoffMultiplied,
                 baseColorFactor
             });
             specialCombos.Add(new List<Property>()
             {
-                alphaCutoffEqual,
-                baseColorFactor
-            });
-            specialCombos.Add(new List<Property>()
-            {
-                alphaCutoffHigh,
+                cutoffHigh,
                 baseColorFactor
             });
             removeCombos.Add(new List<Property>()
@@ -62,18 +52,13 @@ namespace AssetGenerator.ModelGroups
             });
             removeCombos.Add(new List<Property>()
             {
-                color
+                cutoffMultiplied
             });
         }
 
         override public List<List<Property>> ApplySpecialProperties(ModelGroup test, List<List<Property>> combos)
         {
-            var alphaCutoffLow = properties.Find(e => e.name == Propertyname.AlphaCutoff_Low);
-            var alphaCutoffEqual = properties.Find(e => e.name == Propertyname.AlphaCutoff_Equal);
-            var alphaCutoffHigh = properties.Find(e => e.name == Propertyname.AlphaCutoff_High);
-            var baseColorFactor = properties.Find(e => e.name == Propertyname.BaseColorFactor);
             var baseColorTexture = properties.Find(e => e.name == Propertyname.BaseColorTexture);
-            var color = properties.Find(e => e.name == Propertyname.VertexColor_Vector4_Float);
 
             // BaseColorTexture is used everywhere except in the empty set
             foreach (var y in combos)
@@ -88,12 +73,6 @@ namespace AssetGenerator.ModelGroups
                     }
                 }
             }
-
-            // Add more combos last, so they don't have a base color texture
-            combos.Add(new List<Property>()
-            {
-                color
-            });
 
             //// Sort the combos by complexity
             combos.Sort(delegate (List<Property> x, List<Property> y)
