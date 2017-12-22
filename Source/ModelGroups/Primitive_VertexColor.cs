0f25c71c4ab8bf7818dcebaf667ee17b02352d2a
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2017-12-22 12:43:38 -0800

    Use an object to track vertex color values

--- Source/ModelGroups/Primitive_VertexColor.cs
+++ Source/ModelGroups/Primitive_VertexColor.cs
@@ -17,16 +17,33 @@ namespace AssetGenerator.ModelGroups
                 new Vector4( 1.0f, 0.0f, 0.0f, 0.2f),
                 new Vector4( 1.0f, 1.0f, 0.0f, 0.2f),
                 new Vector4( 0.0f, 0.0f, 1.0f, 0.2f)
-                
             };
             properties = new List<Property>
             {
-                new Property(Propertyname.VertexColor_Vector3_Float, vertexColors, group:3),
-                new Property(Propertyname.VertexColor_Vector3_Byte, vertexColors, group:3),
-                new Property(Propertyname.VertexColor_Vector3_Short, vertexColors, group:3),
-                new Property(Propertyname.VertexColor_Vector4_Float, vertexColors, group:3),
-                new Property(Propertyname.VertexColor_Vector4_Byte, vertexColors, group:3),
-                new Property(Propertyname.VertexColor_Vector4_Short, vertexColors, group:3),
+                new Property(Propertyname.VertexColor_Vector3_Float, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC3,
+                    vertexColors)),
+                new Property(Propertyname.VertexColor_Vector3_Byte, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC3,
+                    vertexColors)),
+                new Property(Propertyname.VertexColor_Vector3_Short, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC3,
+                    vertexColors)),
+                new Property(Propertyname.VertexColor_Vector4_Float, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC4,
+                    vertexColors)),
+                new Property(Propertyname.VertexColor_Vector4_Byte, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC4,
+                    vertexColors)),
+                new Property(Propertyname.VertexColor_Vector4_Short, group:3, propertyValue: new VertexColor(
+                    Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT,
+                    Runtime.MeshPrimitive.ColorTypeEnum.VEC4,
+                    vertexColors)),
             };
         }
 
@@ -40,65 +57,18 @@ namespace AssetGenerator.ModelGroups
 
         public Runtime.GLTF SetModelAttributes(Runtime.GLTF wrapper, Runtime.Material material, List<Property> combo, ref glTFLoader.Schema.Gltf gltf)
         {
-            // Remove the base model's UV0 on the empty set
-            if (combo.Count < 0)
-            {
-                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets.RemoveAt(0);
-                material.MetallicRoughnessMaterial = null;
-            }
+            // Remove the base model's UV0
+            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets.RemoveAt(0);
+            material.MetallicRoughnessMaterial = null;
 
             foreach (Property property in combo)
             {
-                switch (property.name)
-                {
-                    case Propertyname.VertexColor_Vector3_Float:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                    case Propertyname.VertexColor_Vector4_Float:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.FLOAT;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                    case Propertyname.VertexColor_Vector3_Byte:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                    case Propertyname.VertexColor_Vector4_Byte:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_UBYTE;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                    case Propertyname.VertexColor_Vector3_Short:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC3;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                    case Propertyname.VertexColor_Vector4_Short:
-                        {
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = Runtime.MeshPrimitive.ColorComponentTypeEnum.NORMALIZED_USHORT;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = Runtime.MeshPrimitive.ColorTypeEnum.VEC4;
-                            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value;
-                            break;
-                        }
-                }
+                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorComponentType = property.value.componentType;
+                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].ColorType = property.value.type;
+                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Colors = property.value.colors;
             }
-            if (combo.Count > 0) // Don't set the material on the empty set
-            {
-                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Material = material;
-            }
+
+            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Material = material;
 
             return wrapper;
         }
--- Source/Property.cs
+++ Source/Property.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Reflection;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace AssetGenerator
 {
@@ -196,6 +198,21 @@ namespace AssetGenerator
             {
                 throw new ArgumentException("Unknown type");
             }
+        }
+    }
+
+    class VertexColor
+    {
+        public Runtime.MeshPrimitive.ColorComponentTypeEnum componentType;
+        public Runtime.MeshPrimitive.ColorTypeEnum type;
+        public List<Vector4> colors;
+
+        public VertexColor(Runtime.MeshPrimitive.ColorComponentTypeEnum colorComponentType,
+                           Runtime.MeshPrimitive.ColorTypeEnum colorType, List<Vector4> vertexColors)
+        {
+            componentType = colorComponentType;
+            type = colorType;
+            colors = vertexColors;
         }
     }
 }
