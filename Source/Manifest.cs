64c4e11d32e3fdc65680951f90d3bd84fa10e416
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2018-02-27 12:12:48 -0800

    Change position to translation

--- Source/Manifest.cs
+++ Source/Manifest.cs
@@ -22,24 +22,24 @@ namespace AssetGenerator
             public string sampleThumbnailName;
             public Camera camera;
 
-            public Model(string name, Vector3 pos, Vector4 rot)
+            public Model(string name, Vector3 trans, Vector4 rot)
             {
                 fileName = name;
                 sampleImageName = "SampleImages" + '/' + name.Replace(".gltf", ".png");
                 sampleThumbnailName = "Thumbnails" + '/' + name.Replace(".gltf", ".png");
-                camera = new Camera(pos, rot);
+                camera = new Camera(trans, rot);
             }
         }
 
         public class Camera
         {
-            public float[] cameraPosition = new float[3];
-            public float[] cameraRotation = new float[4];
+            public float[] translation = new float[3];
+            public float[] rotation = new float[4];
 
-            public Camera(Vector3 pos, Vector4 rot)
+            public Camera(Vector3 trans, Vector4 rot)
             {
-                pos.CopyTo(cameraPosition);
-                rot.CopyTo(cameraRotation);
+                trans.CopyTo(translation);
+                rot.CopyTo(rotation);
             }
         }
     }
