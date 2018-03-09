068aa68eca59f791bc0dbe99e95ff30c14fed0bf
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2018-03-09 15:50:06 -0800

    Initial attempt at adding normals tests to points and lines

--- Source/ModelGroups/Mesh_PrimitiveMode.cs
+++ Source/ModelGroups/Mesh_PrimitiveMode.cs
@@ -11,11 +11,18 @@ namespace AssetGenerator.ModelGroups
             modelGroupName = ModelGroupName.Mesh_PrimitiveMode;
             onlyBinaryProperties = false;
             noPrerequisite = false;
+
             Runtime.Image figureIndices = new Runtime.Image
             {
                 Uri = figures.Find(e => e.Contains("Indices"))
             };
+            Runtime.Image normalTexture = new Runtime.Image
+            {
+                Uri = textures.Find(e => e.Contains("Normal_Plane"))
+            };
+            usedTextures.Add(normalTexture);
             usedFigures.Add(figureIndices);
+
             List<Vector3> noIndicesPositionsTriangles = new List<Vector3>()
             {
                 new Vector3(-0.5f,-0.5f, 0.0f),
@@ -36,12 +43,31 @@ namespace AssetGenerator.ModelGroups
                 new Vector3(-0.5f,-0.5f, 0.0f),
                 new Vector3( 0.5f,-0.5f, 0.0f),
             };
-            List<Vector3> noIndicesPositionsLineloopPointsFan = new List<Vector3>()
+            List<Vector3> noIndicesPositionsLineloopFan = new List<Vector3>()
             {
                 new Vector3( 0.5f,-0.5f, 0.0f),
                 new Vector3( 0.5f, 0.5f, 0.0f),
                 new Vector3(-0.5f, 0.5f, 0.0f),
                 new Vector3(-0.5f,-0.5f, 0.0f),
+            };
+            List<Vector3> noIndicesPositionsPoints = new List<Vector3>()
+            {
+                new Vector3( 0.5f,-0.5f, 0.0f),
+                new Vector3( 0.25f,-0.5f, 0.0f),
+                new Vector3( 0.0f,-0.5f, 0.0f),
+                new Vector3(-0.25f,-0.5f, 0.0f),
+                new Vector3(-0.5f,-0.5f, 0.0f),
+                new Vector3(-0.5f, -0.25f, 0.0f),
+                new Vector3(-0.5f, 0.0f, 0.0f),
+                new Vector3(-0.5f, 0.25f, 0.0f),
+                new Vector3(-0.5f, 0.5f, 0.0f),
+                new Vector3(-0.25f, 0.5f, 0.0f),
+                new Vector3( 0.0f, 0.5f, 0.0f),
+                new Vector3( 0.25f, 0.5f, 0.0f),
+                new Vector3( 0.5f, 0.5f, 0.0f),
+                new Vector3( 0.5f, 0.25f, 0.0f),
+                new Vector3( 0.5f, 0.0f, 0.0f),
+                new Vector3( 0.5f,-0.25f, 0.0f),
             };
             List<Vector3> noIndicesPositionsLineStrip = new List<Vector3>()
             {
@@ -64,7 +90,7 @@ namespace AssetGenerator.ModelGroups
             {
                 0, 3, 3, 2, 2, 1, 1, 0,
             };
-            List<int> lineloopPointsFanIndices = new List<int>
+            List<int> lineloopFanIndices = new List<int>
             {
                 0, 3, 2, 1,
             };
@@ -76,12 +102,51 @@ namespace AssetGenerator.ModelGroups
             {
                 0, 3, 1, 2,
             };
-            List<Vector4> vertexColors = new List<Vector4>()
+            List<int> pointsIndices = new List<int>();
+            for (int x = 0; x < noIndicesPositionsPoints.Count; x++)
             {
-                new Vector4( 0.0f, 1.0f, 0.0f, 0.2f),
-                new Vector4( 1.0f, 0.0f, 0.0f, 0.2f),
-                new Vector4( 0.0f, 0.0f, 1.0f, 0.2f)
+                pointsIndices.Add(x);
+            }
+            
+            List<Vector2> pointsTextureCoords = new List<Vector2>()
+            {
+                new Vector2(1.0f, 1.0f),
+                new Vector2(0.75f, 1.0f),
+                new Vector2(0.5f, 1.0f),
+                new Vector2(0.25f, 1.0f),
+                new Vector2(0.0f, 1.0f),
+                new Vector2(0.0f, 0.75f),
+                new Vector2(0.0f, 0.5f),
+                new Vector2(0.0f, 0.25f),
+                new Vector2(0.0f, 0.0f),
+                new Vector2(0.25f, 0.0f),
+                new Vector2(0.5f, 0.0f),
+                new Vector2(0.75f, 0.0f),
+                new Vector2(1.0f, 0.0f),
+                new Vector2(1.0f, 0.25f),
+                new Vector2(1.0f, 0.5f),
+                new Vector2(1.0f, 0.75f),
             };
+            List<Vector2> linesTextureCoords = new List<Vector2>()
+            {
+                new Vector2(1.0f, 1.0f),
+                new Vector2(0.0f, 1.0f),
+                new Vector2(0.0f, 0.0f),
+                new Vector2(1.0f, 0.0f),
+            };
+            List<List<Vector2>> textureCoords = new List<List<Vector2>>()
+            {
+                pointsTextureCoords,
+                linesTextureCoords
+            };
+            List<Vector3> normals = new List<Vector3>();
+            List<Vector4> tangents = new List<Vector4>();
+            for (int x = 0; x < 16; x++)
+            {
+                normals.Add(new Vector3(0.0f, 0.0f, 1.0f));
+                tangents.Add(new Vector4(1.0f, 0.0f, 0.0f, 1.0f));
+            }
+
             properties = new List<Property>
             {
                 new Property(Propertyname.Mode_Points, Runtime.MeshPrimitive.ModeEnum.POINTS, group: 1),
@@ -91,28 +156,33 @@ namespace AssetGenerator.ModelGroups
                 new Property(Propertyname.Mode_Triangle_Strip, Runtime.MeshPrimitive.ModeEnum.TRIANGLE_STRIP, group: 1),
                 new Property(Propertyname.Mode_Triangle_Fan, Runtime.MeshPrimitive.ModeEnum.TRIANGLE_FAN, group: 1),
                 new Property(Propertyname.Mode_Triangles, Runtime.MeshPrimitive.ModeEnum.TRIANGLES, group: 1),
-                new Property(Propertyname.IndicesValues_Points, lineloopPointsFanIndices, Propertyname.Mode_Points, group: 2),
+                new Property(Propertyname.IndicesValues_Points, pointsIndices, Propertyname.Mode_Points, group: 2),
                 new Property(Propertyname.IndicesValues_Lines, linesIndices, Propertyname.Mode_Lines, group: 2),
-                new Property(Propertyname.IndicesValues_LineLoop, lineloopPointsFanIndices, Propertyname.Mode_Line_Loop, group: 2),
+                new Property(Propertyname.IndicesValues_LineLoop, lineloopFanIndices, Propertyname.Mode_Line_Loop, group: 2),
                 new Property(Propertyname.IndicesValues_LineStrip, linestripIndices, Propertyname.Mode_Line_Strip, group: 2),
                 new Property(Propertyname.IndicesValues_TriangleStrip, trianglestripIndices, Propertyname.Mode_Triangle_Strip, group: 2),
-                new Property(Propertyname.IndicesValues_TriangleFan, lineloopPointsFanIndices, Propertyname.Mode_Triangle_Fan, group: 2),
+                new Property(Propertyname.IndicesValues_TriangleFan, lineloopFanIndices, Propertyname.Mode_Triangle_Fan, group: 2),
                 new Property(Propertyname.IndicesValues_Triangles, defaultModelIndices, Propertyname.Mode_Triangles, group: 2),
                 new Property(Propertyname.IndicesValues_None, " ", group: 2),
                 new Property(Propertyname.IndicesComponentType_Byte, Runtime.MeshPrimitive.IndexComponentTypeEnum.UNSIGNED_BYTE, group: 4),
                 new Property(Propertyname.IndicesComponentType_Short, Runtime.MeshPrimitive.IndexComponentTypeEnum.UNSIGNED_SHORT, group: 4),
                 new Property(Propertyname.IndicesComponentType_Int, Runtime.MeshPrimitive.IndexComponentTypeEnum.UNSIGNED_INT, group: 4),
                 new Property(Propertyname.IndicesComponentType_None, " ", group: 4),
+
+                new Property(Propertyname.VertexUV0_Float, ":white_check_mark:", group:5),
+                new Property(Propertyname.VertexNormal, normals),
+                new Property(Propertyname.VertexTangent, tangents),
+                new Property(Propertyname.NormalTexture, normalTexture),
             };
             specialProperties = new List<Property>
             {
-                new Property(Propertyname.VertexColor_Vector4_Float, vertexColors),
-                new Property(Propertyname.Mode_Points, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.VertexUV0_Float, textureCoords, group:5),
+                new Property(Propertyname.Mode_Points, noIndicesPositionsPoints, group: 1),
                 new Property(Propertyname.Mode_Lines, noIndicesPositionsLines, group: 1),
-                new Property(Propertyname.Mode_Line_Loop, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.Mode_Line_Loop, noIndicesPositionsLineloopFan, group: 1),
                 new Property(Propertyname.Mode_Line_Strip, noIndicesPositionsLineStrip, group: 1),
                 new Property(Propertyname.Mode_Triangle_Strip, noIndicesPositionsTrianglestrip, group: 1),
-                new Property(Propertyname.Mode_Triangle_Fan, noIndicesPositionsLineloopPointsFan, group: 1),
+                new Property(Propertyname.Mode_Triangle_Fan, noIndicesPositionsLineloopFan, group: 1),
                 new Property(Propertyname.Mode_Triangles, noIndicesPositionsTriangles, group: 1),
             };
             // Each mode with and without indices, and drop singles
@@ -136,14 +206,55 @@ namespace AssetGenerator.ModelGroups
                 removeCombos.Add(ComboHelper.CustomComboCreation(
                     property));
             }
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.IndicesValues_Triangles),
-                properties.Find(e => e.name == Propertyname.IndicesComponentType_Byte),
-                properties.Find(e => e.name == Propertyname.Mode_Triangles)));
-            specialCombos.Add(ComboHelper.CustomComboCreation(
-                properties.Find(e => e.name == Propertyname.IndicesValues_Triangles),
-                properties.Find(e => e.name == Propertyname.IndicesComponentType_Short),
-                properties.Find(e => e.name == Propertyname.Mode_Triangles)));
+
+            var valueTriangles = properties.Find(e => e.name == Propertyname.IndicesValues_Triangles);
+            var modeTriangles = properties.Find(e => e.name == Propertyname.Mode_Triangles);
+            var modePoints = properties.Find(e => e.name == Propertyname.Mode_Points);
+            var modeLines = properties.Find(e => e.name == Propertyname.Mode_Lines);
+            var typeByte = properties.Find(e => e.name == Propertyname.IndicesComponentType_Byte);
+            var typeShort = properties.Find(e => e.name == Propertyname.IndicesComponentType_Short);
+            var vertexUV = properties.Find(e => e.name == Propertyname.VertexUV0_Float);
+            var normal = properties.Find(e => e.name == Propertyname.VertexNormal);
+            var tangent = properties.Find(e => e.name == Propertyname.VertexTangent);
+            var normalTex = properties.Find(e => e.name == Propertyname.NormalTexture);
+            specialCombos.Add(new List<Property>()
+            {
+                valueTriangles,
+                typeByte,
+                modeTriangles
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                valueTriangles,
+                typeShort,
+                modeTriangles
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                modePoints,
+                noIndicesValue,
+                noIndicesType,
+                vertexUV,
+                normal
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                modeLines,
+                noIndicesValue,
+                noIndicesType,
+                vertexUV,
+                normal
+            });
+            specialCombos.Add(new List<Property>()
+            {
+                modeLines,
+                noIndicesValue,
+                noIndicesType,
+                vertexUV,
+                normal,
+                tangent,
+                normalTex
+            });
         }
 
         override public List<List<Property>> ApplySpecialProperties(ModelGroup test, List<List<Property>> combos)
@@ -154,15 +265,36 @@ namespace AssetGenerator.ModelGroups
                 combos.RemoveAt(0);
             }
 
-            // Sorts the models so all of the ones without indices are together in the MD log
+            // Sort the combos by complexity
             combos.Sort(delegate (List<Property> x, List<Property> y)
             {
-                var xIndices = x.Find(e => e.name == Propertyname.IndicesValues_None);
-                var yIndices = y.Find(e => e.name == Propertyname.IndicesValues_None);
-
-                if (xIndices == null && yIndices == null) return 0;
-                else if (xIndices == null) return 1;
-                else if (yIndices == null) return -1;
+                if (x.Count == 0) return -1; // Empty Set
+                else if (y.Count == 0) return 1; // Empty Set
+                else if (x.Count > y.Count) return 1;
+                else if (x.Count < y.Count) return -1;
+                else if (x.Count == y.Count)
+                {
+                    // Tie goes to the combo with the left-most property on the table
+                    for (int p = 0; p < x.Count; p++)
+                    {
+                        if (x[p].propertyGroup != y[p].propertyGroup ||
+                            x[p].propertyGroup == 0)
+                        {
+                            int xPropertyIndex = properties.FindIndex(e => e.name == x[p].name);
+                            int yPropertyIndex = properties.FindIndex(e => e.name == y[p].name);
+                            if (xPropertyIndex > yPropertyIndex) return 1;
+                            else if (xPropertyIndex < yPropertyIndex) return -1;
+                        }
+                    }
+                    for (int p = 0; p < x.Count; p++)
+                    {
+                        int xPropertyIndex = properties.FindIndex(e => e.name == x[p].name);
+                        int yPropertyIndex = properties.FindIndex(e => e.name == y[p].name);
+                        if (xPropertyIndex > yPropertyIndex) return 1;
+                        else if (xPropertyIndex < yPropertyIndex) return -1;
+                    }
+                    return 0;
+                }
                 else return 0;
             });
 
@@ -240,9 +372,39 @@ namespace AssetGenerator.ModelGroups
                     var modeVertexes = specialProperties.Find(e => e.name == mode.name);
                     wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Positions = modeVertexes.value;
                 }
+                else if (property.name == Propertyname.VertexUV0_Float)
+                {
+                    List<List<Vector2>> texCoords = new List<List<Vector2>>();
+                    if (combo.Find(e => e.name == Propertyname.Mode_Points) != null)
+                    {
+                        texCoords = specialProperties.Find(e => e.name == Propertyname.VertexUV0_Float).value[0];
+                    }
+                    else // Mode_Lines
+                    {
+                        texCoords = specialProperties.Find(e => e.name == Propertyname.VertexUV0_Float).value[1];
+                    }
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets = texCoords;
+                }
+                else if (property.name == Propertyname.VertexNormal)
+                {
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Normals = property.value;
+                }
+                else if (property.name == Propertyname.VertexTangent)
+                {
+                    wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].Tangents = property.value;
+                }
+                else if (property.name == Propertyname.NormalTexture)
+                {
+                    material.NormalTexture = new Runtime.Texture();
+                    material.NormalTexture.Source = property.value;
+                    material.NormalTexture.TexCoordIndex = 0;
+                }
             }
 
-            wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets = null;
+            if (material.NormalTexture == null)
+            {
+                wrapper.Scenes[0].Nodes[0].Mesh.MeshPrimitives[0].TextureCoordSets = null;
+            }
 
             return wrapper;
         }
