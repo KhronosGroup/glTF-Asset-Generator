fc240ee603f6396a7fe9bfeaddc9f0632fcb0208
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2018-05-03 08:11:44 -0700

    Cleanup

--- Source/ModelGroups/Material_Mixed.cs
+++ Source/ModelGroups/Material_Mixed.cs
@@ -15,14 +15,12 @@ namespace AssetGenerator
             GetImage(imageList, "UVSpace3", isFigure: true);
 
             // Track the common properties for use in the readme.
-            var baseColorTextureValue = new Vector4(0.2f, 0.2f, 0.2f, 1.0f);
             CommonProperties.Add(new Property(PropertyName.ExtensionUsed, "Specular Glossiness"));
             CommonProperties.Add(new Property(PropertyName.BaseColorTexture, baseColorTextureImage));
 
             Model CreateModel(Action<List<Property>, Runtime.Material, Runtime.Material> setProperties)
             {
                 var properties = new List<Property>();
-                var extensions = new List<Runtime.Extensions.Extension>();
                 var meshPrimitives = MeshPrimitive.CreatePlaneFromTriangles();
                 meshPrimitives[0].Material = new Runtime.Material();
                 meshPrimitives[0].Material.MetallicRoughnessMaterial = new Runtime.PbrMetallicRoughness();
@@ -60,13 +58,13 @@ namespace AssetGenerator
                 };
             }
 
-            void SetSpecularGlossiness0(List<Property> properties, Runtime.Material material0, Runtime.Material material1)
+            void SetSpecularGlossiness0(List<Property> properties, Runtime.Material material0)
             {
                 material0.Extensions = new List<Runtime.Extensions.Extension>() { new Runtime.Extensions.PbrSpecularGlossiness() }; ;
                 properties.Add(new Property(PropertyName.SpecularGlossinessOnMaterial0, ":white_check_mark:", group: 1));
             }
 
-            void SetSpecularGlossiness1(List<Property> properties, Runtime.Material material0, Runtime.Material material1)
+            void SetSpecularGlossiness1(List<Property> properties, Runtime.Material material1)
             {
                 material1.Extensions = new List<Runtime.Extensions.Extension>() { new Runtime.Extensions.PbrSpecularGlossiness() }; ;
                 properties.Add(new Property(PropertyName.SpecularGlossinessOnMaterial1, ":white_check_mark:", group: 2));
@@ -85,15 +83,15 @@ namespace AssetGenerator
             this.Models = new List<Model>
             {
                 CreateModel((properties, material0, material1) => {
-                    SetSpecularGlossiness0(properties, material0, material1);
-                    SetSpecularGlossiness1(properties, material0, material1);
+                    SetSpecularGlossiness0(properties, material0);
+                    SetSpecularGlossiness1(properties, material1);
                 }),
                 CreateModel((properties, material0, material1) => {
                     NoSpecularGlossiness0(properties);
                     NoSpecularGlossiness1(properties);
                 }),
                 CreateModel((properties, material0, material1) => {
-                    SetSpecularGlossiness0(properties, material0, material1);
+                    SetSpecularGlossiness0(properties, material0);
                     NoSpecularGlossiness1(properties);
 
                 }),
