90db40688dde7aeb6aa1917f40fd4e29c73d5ad8
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2019-01-25 16:53:26 -0800

    Specify encoding type for log file

--- ValidateModels.ps1
+++ ValidateModels.ps1
@@ -6,5 +6,5 @@ $logFile = Join-Path -Path ".." -ChildPath $outputFolder |Join-Path -ChildPath "
 Set-Location "Validator"
 Write-Host "Validating models in manifest:"
 Resolve-Path -Path $manifestPath | Write-Host
-npm start -- "manifest=$manifestPath" | Out-File $logFile
+npm start -- "manifest=$manifestPath" | Out-File $logFile -Encoding utf8
 Set-Location ..
