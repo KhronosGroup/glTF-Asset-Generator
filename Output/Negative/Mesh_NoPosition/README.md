a044ee005de76eb6ce1704220dd57ff19ee49c44
Steven Kopsa <31048895+stevk@users.noreply.github.com>
2019-05-31 10:01:35 -0700

    Implement no position models

--- Output/Negative/Mesh_NoPosition/README.md
+++ Output/Negative/Mesh_NoPosition/README.md
@@ -4,15 +4,14 @@ The following table shows the properties that are set for every model.
 
 | Property | **Values** |
 | :---: | :---: |
-| Position | No `POSITION` attribute is set |
 
 
 <br>
 
 The following table shows the properties that are set for a given model.  
 
-|   | Sample Image | Indices | Vertex Normal |
-| :---: | :---: | :---: | :---: |
-| [00](Mesh_NoPosition_00.gltf)<br>[View](https://bghgary.github.io/glTF-Assets-Viewer/?type=Negative&folder=22&model=0) | [<img src="Figures/Thumbnails/Mesh_NoPosition_00.png" align="middle">](Figures/SampleImages/Mesh_NoPosition_00.png) |   | :white_check_mark: |
-| [01](Mesh_NoPosition_01.gltf)<br>[View](https://bghgary.github.io/glTF-Assets-Viewer/?type=Negative&folder=22&model=1) | [<img src="Figures/Thumbnails/Mesh_NoPosition_01.png" align="middle">](Figures/SampleImages/Mesh_NoPosition_01.png) | [1, 0, 3, 1, 3, 2] | :white_check_mark: |
+|   | Sample Image | Indices | Position | Vertex Normal |
+| :---: | :---: | :---: | :---: | :---: |
+| [00](Mesh_NoPosition_00.gltf)<br>[View](https://bghgary.github.io/glTF-Assets-Viewer/?type=Negative&folder=22&model=0) | [<img src="Figures/Thumbnails/Mesh_NoPosition_00.png" align="middle">](Figures/SampleImages/Mesh_NoPosition_00.png) |   | :x: | :white_check_mark: |
+| [01](Mesh_NoPosition_01.gltf)<br>[View](https://bghgary.github.io/glTF-Assets-Viewer/?type=Negative&folder=22&model=1) | [<img src="Figures/Thumbnails/Mesh_NoPosition_01.png" align="middle">](Figures/SampleImages/Mesh_NoPosition_01.png) | [0, 1, 2] | :x: | :white_check_mark: |
  
--- Source/ModelGroup_Triangle.cs
+++ Source/ModelGroup_Triangle.cs
@@ -7,7 +7,7 @@ namespace AssetGenerator
     {
         protected static partial class Mesh
         {
-            public static Runtime.Mesh CreateTriangle()
+            public static Runtime.Mesh CreateTriangle(bool includeMaterial = true, bool includeIndices = true, bool includePositions = true, bool includeNormals = false)
             {
                 return new Runtime.Mesh
                 {
@@ -16,26 +16,52 @@ namespace AssetGenerator
                     {
                         new Runtime.MeshPrimitive
                         {
-                            Positions = new List<Vector3>()
-                            {
-                                new Vector3(0.0f, -0.2f, -0.05f),
-                                new Vector3(0.0f, -0.2f,  0.05f),
-                                new Vector3(0.0f,  0.0f,  0.00f),
-                            },
-                            Indices = new List<int>
-                            {
-                                0, 1, 2,
-                            },
-                            Material = new Runtime.Material
-                            {
-                                DoubleSided = true,
-                                MetallicRoughnessMaterial = new Runtime.PbrMetallicRoughness
-                                {
-                                    BaseColorFactor = new Vector4(0.0f, 0.0f, 1.0f, 1.0f)
-                                }
-                            }
+                            Positions = includePositions ? GetTrianglePositions() : null,
+                            Indices = includeIndices ? GetTriangleIndices() : null,
+                            Material = includeMaterial ? GetTriangleMaterial() : null,
+                            Normals = includeNormals ? GetTriangleNormals() : null,
                         }
                     }
+                };
+            }
+
+            public static List<Vector3> GetTrianglePositions()
+            {
+                return new List<Vector3>()
+                {
+                    new Vector3(0.0f, -0.2f, -0.05f),
+                    new Vector3(0.0f, -0.2f,  0.05f),
+                    new Vector3(0.0f,  0.0f,  0.00f),
+                };
+            }
+
+            public static Runtime.Material GetTriangleMaterial()
+            {
+                return new Runtime.Material
+                {
+                    DoubleSided = true,
+                    MetallicRoughnessMaterial = new Runtime.PbrMetallicRoughness
+                    {
+                        BaseColorFactor = new Vector4(0.0f, 0.0f, 1.0f, 1.0f)
+                    }
+                };
+            }
+
+            public static List<int> GetTriangleIndices()
+            {
+                return new List<int>
+                {
+                    0, 1, 2,
+                };
+            }
+
+            public static List<Vector3> GetTriangleNormals()
+            {
+                return new List<Vector3>()
+                {
+                    new Vector3(0.0f, 0.0f, 1.0f),
+                    new Vector3(0.0f, 0.0f, 1.0f),
+                    new Vector3(0.0f, 0.0f, 1.0f),
                 };
             }
         }
