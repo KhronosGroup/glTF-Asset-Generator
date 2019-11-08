903c76b2ba3d76195add122ece65e2c0983422db
YuZhang <zy_a_zy@163.com>
2019-11-08 17:34:03 +0800

    Add negative scale to node.

--- Source/ModelGroups/Instancing.cs
+++ Source/ModelGroups/Instancing.cs
@@ -358,6 +358,20 @@ namespace AssetGenerator.ModelGroups
                 }, (model) => { model.Camera = distantCamera; }),
                 CreateModel((properties, nodes, animations) =>
                 {
+                    var meshPrimitive = MeshPrimitive.CreateSinglePlane();
+                    meshPrimitive.Material = CreateMaterial(CreateTextureInfo(baseColorImageA));
+
+                    AddMeshPrimitivesToMultipleNodes(nodes, meshPrimitive, meshPrimitive);
+                    nodes[1].Mesh = nodes[0].Mesh;
+
+                    // Adds just the node containing the negative scale.
+                    nodes[0].Scale = new Vector3(-1.0f, -1.0f, 1.0f);
+
+                    properties.Add(new Property(PropertyName.Description, "Two nodes using the same mesh."));
+                    properties.Add(new Property(PropertyName.Difference, "One node has a negative scale and the other does not have a negative scale."));
+                }, (model) => { model.Camera = distantCamera; }),
+                CreateModel((properties, nodes, animations) =>
+                {
                     nodes.AddRange(Nodes.CreateFoldingPlaneSkin("skinA", 2, 3));
                     nodes[0].Name = "plane0";
                     nodes[0].Mesh.MeshPrimitives.ElementAt(0).Material = CreateMaterial(CreateTextureInfo(baseColorImageA));
